139ea77e8a9c0fd1f34224565d58418e92928d73
mhb <mhb.fallahi@gmail.com>
2021-05-30 01:38:16 +0430

    decide which fragment to show

--- app/src/main/java/com/example/pars/uni_prj/main/buy.java
+++ app/src/main/java/com/example/pars/uni_prj/main/buy.java
@@ -1,21 +1,37 @@
 package com.example.pars.uni_prj.main;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.pars.uni_prj.R;
 
+import java.util.Objects;
+
 public class buy extends AppCompatActivity {
 
-    final static String urlAddress = "http://192.168.1.5/uni/img.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.buy_container,new orders()).commit();
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        int value= bundle.getInt("search");
+        if (value==1){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.buy_container,new searchFragment()).commit();
+        }else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.buy_container,new orders()).commit();
+        }
 
 
     }
+
+
 }
